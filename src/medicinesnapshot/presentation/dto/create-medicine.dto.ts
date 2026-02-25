b73a8324af8457cf5b5c2a327286e9a52ca3412c
MossOcelot <phuminforwork@gmail.com>
2026-02-25 15:06:41 +0700

    feat: add MedicineCreatedEventDto interface for event handling

--- src/medicinesnapshot/presentation/dto/create-medicine.dto.ts
+++ src/medicinesnapshot/presentation/dto/create-medicine.dto.ts
@@ -5,3 +5,13 @@ export interface CreateMedicineSnapshotDto {
   medicineName_en?: string;
   medicineName_th?: string;
 }
+
+export interface MedicineCreatedEventDto {
+  medicineId: string;
+  roomId: string;
+  info: {
+    medicineCode: string;
+    medicineName_en?: string;
+    medicineName_th?: string;
+  };
+}
